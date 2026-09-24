855380daf6f8c1bc413de14adefdc22492760510
bitofbogan <bitofbogan66@gmail.com>
2026-09-23 18:17:03 -0600

    Update m14.js

--- aureka/data/m14.js
+++ aureka/data/m14.js
@@ -233,7 +233,7 @@ var questionBank = [
     id: 14, //READY
     topicId: "interes_simple_tiempo",
     type: "choice",
-    prompt: "¿En cuántos meses se obtendrá un monto final de $120,000 a partir de una inversión de $40,000 con una tasa de interés simple del 5% bimestral?",
+    prompt: "¿En cuántos meses se obtendrá un monto final de $\\text{\\$120,000}$ a partir de una inversión de $\\text{\\$40,000}$ con una tasa de interés simple del 5% bimestral?",
     image: null,
     hint: "El interés que se busca ganar es la diferencia ($80,000). Despeja el número de plazos bimestrales con $$t = \\frac{I}{C \\cdot i}$$ y luego convierte los plazos a meses.",
     correct: "80 meses.",
@@ -319,7 +319,7 @@ var questionBank = [
     id: 19, //READY
     topicId: "ingreso_base_mensual",
     type: "choice",
-    prompt: "Roberto percibe un salario mensual de $7,500, mientras que su esposa cobra $150 por jornada trabajando entre 3 y 5 días a la semana. ¿Cuál es el ingreso base mensual de la pareja? ¿Por qué?",
+    prompt: "Roberto percibe un salario mensual de $\\text{\\$7,500}, mientras que su esposa cobra $\\text{\\$150}$ por jornada trabajando entre 3 y 5 días a la semana. ¿Cuál es el ingreso base mensual de la pareja? ¿Por qué?",
     image: null,
     hint: "El ingreso base mensual toma en consideración únicamente aquellas fuentes de ingreso estables y garantizadas.",
     correct: "$7,500.00, porque el IBM se calcula estrictamente con la suma de los ingresos fijos.",
